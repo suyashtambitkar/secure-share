8df1b0fe1d2363549e366b5c4b8a7e5abceb6f33
suyashtambitkar <suyashtambitkar9@gmail.com>
2025-08-01 20:02:32 +0530

    socketio added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const cors = require('cors');
+const PORT = 5000;
+const {Server} =require('socket.io');
+
+const server = http.createServer(app);
 
 app.use(express.json())
+app.use(cors());
+
+// Create a new Socket.IO server
+const io = new Server(server,{
+    cors:{
+        origin:'*',
+        methods:['Get','Post']
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Backend is running!');
 });
 
-const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
